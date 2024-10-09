336c7940ee96b4ba64566ff723b69cac49e761d6
ArmineKhachatryan1982 <armine.khachatryan1982@gmail.com>
2024-10-09 15:50:12 +0400

    finish forget password

---
+++ resources/views/vendor/notifications/email.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Բարի գալուստ Մուտքի համակարգեր</title>
+</head>
+<body style="margin: 0; padding: 0; background-color: #f9f9f9;">
+    <table width="100%" cellpadding="0" cellspacing="0" border="0">
+        <tr>
+            <td align="center" style="padding: 20px;">
+                <table width="600px" cellpadding="0" cellspacing="0" border="0" style="max-width: 600px; background-color: white;">
+                    <tr>
+                        <td style="padding: 20px; text-align: left;">
+                            {{-- <img src="https://manage.museumsarmenia.am/assets/img/mail-img/mobile-museum-logo.png" alt="Museum Logo" style="width: 130px; height: auto;"> --}}
+                        </td>
+                    </tr>
+                    <tr>
+                        <td style="padding: 20px; text-align: center;">
+                            {{-- <img src="https://manage.museumsarmenia.am/assets/img/mail-img/hh.png" alt="HH-Zinanshan" style="width: 54px; height: 54px;">
+                            <p style="text-align:center" >ՀԱՅԱՍՏԱՆԻ ՀԱՆՐԱՊԵՏՈՒԹՅԱՆ ԿՐԹՈՒԹՅԱՆ, ԳԻՏՈՒԹՅԱՆ, ՄՇԱԿՈՒՅԹԻ ԵՎ ՍՊՈՐՏԻ
+                                ՆԱԽԱՐԱՐՈՒԹՅՈՒՆ</p> --}}
+                        </td>
+                    </tr>
+                    <tr style="text-align:center" ><a href="{{ $actionUrl }}">Գաղտնաբառի վերականգնման համար անցնել հետևյալ հղումով </a></tr>
+                    <p></p>
+                    <p></p>
+                    <p></p>
+                    <p></p>
+                    <p></p>
+                    <p></p>
+                    <p></p>
+                    <p></p>
+                    <p></p>
+                    <p></p>
+                    <p></p>
+                </table>
+          </tr>
+    </table>
+  </body>
+</html>
