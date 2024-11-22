0f1faaa59640d544e9ca29427d71d36683c6fb3e
ArmineKhachatryan1982 <armine.khachatryan1982@gmail.com>
2024-11-22 15:27:48 +0400

    all correct 22.11.24

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -27,7 +27,7 @@ class ReportController extends Controller
         $client = Client::where('user_id', Auth::id())->with('people.attendance_sheets')->first();
         $client_working_day_times = ClientWorkingDayTime::where('client_id',$client->id)->select('week_day','day_start_time','day_end_time','break_start_time','break_end_time')->get();
         $people = $client->people->pluck('id');
-      
+
         if($request->mounth!=null){
             [$year, $month] = explode('-', $request->mounth);
 
@@ -39,7 +39,12 @@ class ReportController extends Controller
             ->get();
             // ->paginate(1);
 
-            $attendant = AttendanceSheet::whereIn('people_id',$people)->get();
+            $attendant = AttendanceSheet::whereIn('people_id',$people)
+            ->whereYear('date', $year)
+            ->whereMonth('date', $month)
+            ->orderBy('date','asc')
+            ->get();
+            // dd($attendant);
 
             $mounth = $request->mounth;
 
@@ -50,7 +55,7 @@ class ReportController extends Controller
         }
         $i=0;
 
-        return view('report.index_new',compact('data','mounth','attendant','client_working_day_times','client','i'));
+        return view('report.index',compact('data','mounth','attendant','client_working_day_times','client','i'));
         // return view('report.index',compact('data','mounth','attendant'))
         // ->with('i', ($request->input('page', 1) - 1) * 1);
 
