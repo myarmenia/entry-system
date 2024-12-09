bd29df30b4618c2bcfdccab47a1c77178814977b
ArmineKhachatryan1982 <armine.khachatryan1982@gmail.com>
2024-12-09 07:52:23 +0400

    ushacum

--- app/Traits/ReportTrait.php
+++ app/Traits/ReportTrait.php
@@ -52,8 +52,6 @@ trait ReportTrait{
 
 
                 $totalWorkingTimePerPerson = [];
-                $daysWorkedPerPerson = [];
-                $delayTimesPerPerson = [];
                 $peopleDailyRecord=[];
 
                 foreach ($groupedEntries as $peopleId => $dailyRecords) {
@@ -61,7 +59,7 @@ trait ReportTrait{
                     $daysWorked = 0; // Количество отработанных дней
                     $totalDelayTime = 0; // Время задержки (в секундах)
                     $delay_arr = [];
-                    $daily_anomalia=[];
+
 
                     // dd($dailyRecords);
                     foreach ($dailyRecords as $date => $records) {
@@ -77,87 +75,94 @@ trait ReportTrait{
                         $clientSchedule = $clientWorkingTimes[$dayOfWeek] ?? null;
 
 
-                        // foreach ($records as $record) {
+                        foreach ($records as $record) {
 
 
-                        //     if ($record->direction === 'enter') {
-                        //         // dd('enter');
-                        //         $entryTime  = Carbon::parse($record->date);
+                            if ($record->direction == 'enter' && !$entryTime ) {
+                                // dd('enter');
+                                $entryTime  = Carbon::parse($record->date);
 
 
-                        //     } elseif ($record->direction === 'exit' && $entryTime) {
-                        //         // dd('enter and exit');
+                            } elseif ($record->direction == 'exit' && $entryTime) {
+                                // dd('enter and exit');
 
-                        //         $exitTime = Carbon::parse($record->date);
+                                $exitTime = Carbon::parse($record->date);
 
-                        //             $entry = explode(' ', $entryTime->toTimeString())[0];
-                        //             $entryT = Carbon::createFromFormat('H:i:s', $entry);
-
-
-                        //             $exit = explode(' ', $exitTime->toTimeString())[0];
-                        //             $exitT = Carbon::createFromFormat('H:i:s', $exit);
-                        //             // dd($entryT, $exitT );
-                        //             // $entryT = Carbon::createFromFormat('H:i:s', '15:11:44');
-                        //             // $exitT = Carbon::createFromFormat('H:i:s', '15:20:41');
+                                    $entry = explode(' ', $entryTime->toTimeString())[0];
+                                    $entryT = Carbon::createFromFormat('H:i:s', $entry);
 
 
-                        //             // Если вход и выход в один день, добавляем разницу
-                        //             if ($exitT->greaterThan($entryT)) {
-                        //                 $interval = $exitT->diff($entryT);
+                                    $exit = explode(' ', $exitTime->toTimeString())[0];
+                                    $exitT = Carbon::createFromFormat('H:i:s', $exit);
 
-                        //                 $peopleDailyRecord[$peopleId][$date]['working_times'] = $interval->format('%H:%I:%S');
-
-
-                        //                 // dd($dailyWorkingTime);
-                        //             }
-
-                        //         // Сбрасываем время входа после расчета
-                        //         $entryTime = null;
+                                    // dump($entryT, $exitT );
+                                    // $entryT = Carbon::createFromFormat('H:i:s', '15:11:44');
+                                    // $exitT = Carbon::createFromFormat('H:i:s', '15:20:41');
 
 
-                        //     }else if($record->direction === 'exit'){
+                                    // Если вход и выход в один день, добавляем разницу
+                                    if ($exitT->greaterThan($entryT)) {
+                                        $interval = $exitT->diff($entryT);
 
-                        //         $daily_anomalia[$date] = false;
+                                        $peopleDailyRecord[$peopleId][$date]['working_times'][] = $interval->format('%H:%I:%S');
+                                        // dump($peopleDailyRecord);
 
-                        //     }
-                        // }
-                        // dd($records);
-                        $worker_first_enter=$records->where('direction', 'enter')->first();
-                        // dd($worker_first_enter);
-                        if($worker_first_enter){
-
-
-
-                                // dd($clientSchedule);
-                                $get_client_week_working_start_time =new DateTime($clientSchedule->day_start_time);
-                                $get_client_week_working_end_time =new DateTime($clientSchedule->day_end_time);
-                                // dd($get_client_week_working_end_time);
-                                $worker_first_enter_time = explode(' ', $worker_first_enter->date)[1];
-                                // dd($worker_first_enter_time);
-
-                                $worker_first_enter_time = new DateTime($worker_first_enter_time);
-                                // dd($get_client_week_working_end_time, $worker_first_enter_time);
-                                if($worker_first_enter_time<$get_client_week_working_end_time){
-
-                                    if($worker_first_enter_time>$get_client_week_working_start_time){
-
-                                        $interval = $worker_first_enter_time->diff($get_client_week_working_start_time);
-
-                                        $peopleDailyRecord[$peopleId][$date]['delay_hour']=$interval->format('%h hours, %i minutes, %s seconds');
-                                        $peopleDailyRecord[$peopleId][$date]['delay_display']=true;
-
+                                        // dd($dailyWorkingTime);
                                     }
 
-                                }else{
-                                    $peopleDailyRecord[$peopleId][$date]['anomalia']=true;
+                                // Сбрасываем время входа после расчета
+                                $entryTime = null;
 
-                                }
-                        }else{
-                            $peopleDailyRecord[$peopleId][$date]['anomalia']=true;
+
+                            }else if($record->direction === 'exit'){
+
+
+                                $peopleDailyRecord[$peopleId][$date]['daily_anomalia'] = false;
+
+                            }
                         }
+                        // dd($peopleDailyRecord);
+                        // dd($records);
+                        $worker_first_enter = $records->where('direction', 'enter')->first();
+                        // dd($worker_first_enter);
+                            if($worker_first_enter){
 
-                    $breakfastInterval = $records
-                        ->filter(function ($record) use ($clientSchedule) {
+
+
+                                    // dd($clientSchedule);
+                                    $get_client_week_working_start_time = new DateTime($clientSchedule->day_start_time);
+
+                                    $get_client_week_working_end_time = new DateTime($clientSchedule->day_end_time);
+                                    // dd($get_client_week_working_end_time);
+                                    $worker_first_enter_time = explode(' ', $worker_first_enter->date)[1];
+                                    // dd($worker_first_enter_time);
+
+                                    $worker_first_enter_time = new DateTime($worker_first_enter_time);
+                                    // dd($get_client_week_working_end_time, $worker_first_enter_time);
+                                    if($worker_first_enter_time<$get_client_week_working_end_time){
+
+                                        // dd($worker_first_enter_time,$get_client_week_working_start_time);
+                                        if($worker_first_enter_time>$get_client_week_working_start_time){
+                                            // dd($get_client_week_working_start_time);
+
+                                            $interval = $worker_first_enter_time->diff($get_client_week_working_start_time);
+
+                                            $peopleDailyRecord[$peopleId][$date]['delay_hour'][]=$interval->format('%h hours, %i minutes, %s seconds');
+                                            $peopleDailyRecord[$peopleId][$date]['delay_display']=true;
+
+                                        }
+
+                                    }else{
+                                        $peopleDailyRecord[$peopleId][$date]['anomalia']=true;
+
+                                    }
+                            }else{
+                                $peopleDailyRecord[$peopleId][$date]['anomalia']=true;
+                            }
+                        // dd($peopleDailyRecord);
+
+                        $breakfastInterval = $records
+                            ->filter(function ($record) use ($clientSchedule) {
                             $recordTime = (new DateTime($record->date))->format('H:i:s');
                             return $recordTime >= $clientSchedule->break_start_time && $recordTime <= $clientSchedule->break_end_time;
                         })
@@ -167,12 +172,11 @@ trait ReportTrait{
                             return $group->first()->date; // Take the first (latest) record's date from each group
                         });
                         $ushacum=false;
-                    dd($breakfastInterval);
+                    // dd($breakfastInterval);
                         if(count($breakfastInterval)>0){
 
                             if(count($breakfastInterval)==1 && isset($breakfastInterval["exit"])){
                                 $ushacum=true;
-
 
                             }
                             if(count($breakfastInterval)>1 ){
@@ -180,59 +184,19 @@ trait ReportTrait{
                                 $enterTime = new DateTime($breakfastInterval['enter']);
                                     $exitTime = new DateTime($breakfastInterval['exit']);
                                     // dump($enterTime,$exitTime);
-
-
                                     if ($exitTime > $enterTime) {
                                         $ushacum=true;
-                                        // dump($delay_arr);
 
                                     }
 
-
+                            }
+                            if($ushacum == true){
+                                $this->ushacum($peopleId, $date, $clientSchedule, $peopleDailyRecord);
 
                             }
-                            if($ushacum==true){
-                                
-
-                            }
-
-
 
                         }
 
-
-
-
-                        // Check if there's a lunch break to subtract
-
-                        // $dayOfWeek = Carbon::parse($date)->format('l');
-                        // $clientSchedule = $clientWorkingTimes[$dayOfWeek] ?? null;
-
-                        // if ($clientSchedule) {
-                        //     $breakStart = Carbon::parse("$date {$clientSchedule->break_start_time}");
-                        //     $breakEnd = Carbon::parse("$date {$clientSchedule->day_end_time}");
-
-                        //     if ($entryTime && $entryTime->lessThan($breakEnd)) {
-                        //         $dailyWorkingTime -= min($breakEnd->diffInSeconds($breakStart), $breakEnd->diffInSeconds($entryTime));
-                        //     }
-                        // }
-
-                        // Add working time for the day
-                        // $totalWorkingTime += $dailyWorkingTime;
-
-                        // // Count the day as worked if there is at least one entry and exit
-                        // if ($dailyWorkingTime > 0) {
-                        //     $daysWorked++;
-                        // }
-
-                        // Calculate delay if entryTime exists
-                        // if ($entryTime) {
-                        //     $expectedStart = Carbon::parse("$date {$clientSchedule->start_time}");
-                        //     if ($entryTime->greaterThan($expectedStart)) {
-                        //         $delayTime = $entryTime->diffInSeconds($expectedStart); // Calculate delay in seconds
-                        //         $totalDelayTime += $delayTime;
-                        //     }
-                        // }
                     }
 
 
@@ -240,50 +204,51 @@ trait ReportTrait{
                 }
 
 
-                // Debugging output (can be removed after testing)
-                // dd($totalWorkingTimePerPerson, $daysWorkedPerPerson, $delayTimesPerPerson);
 
-// dd($groupedEntries);
                     return  $groupedEntries;
 
         }
 
 
     }
-    public function ushacum(){
+    public function ushacum($peopleId, $date, $clientSchedule, $peopleDailyRecord){
 
 
 
             $firstAfter1400 = DB::table('attendance_sheets')
                 ->where('direction', 'enter')
-                ->where('people_id', $at->people_id)
-                ->whereDate('date', date('Y-m-d', strtotime($at->date)))
-                ->whereTime('date', '>', $day_time->break_end_time) // Время после 14:00
+                ->where('people_id', $peopleId)
+                ->whereDate('date', date('Y-m-d', strtotime($date)))
+                ->whereTime('date', '>', $clientSchedule->break_end_time) // Время после 14:00
                 ->orderBy('date', 'asc') // Сортируем по времени
                 ->first();
-
+                    // dd($firstAfter1400);
 
                 if($firstAfter1400){
 
 
                     $firstAfter1400_datePart = explode(' ', $firstAfter1400->date)[1];
+                    // dd($firstAfter1400_datePart);
 
                                 $firstAfter1400_time1 = new DateTime($firstAfter1400_datePart);
 
-                                $firstAfter1400_time2 = new DateTime($day_time->break_end_time);
+                                $firstAfter1400_time2 = new DateTime($clientSchedule->break_end_time);
 
                                 $firstAfter1400_interval = $firstAfter1400_time1 ->diff($firstAfter1400_time2);
+                                // dd($firstAfter1400_interval);
 
                     if($firstAfter1400_interval->format('%H h %I m')!=="00 h 00 m"){
-
-                        $delay_arr[] = $firstAfter1400_interval->format('%H h %I m');
-                        $delay_color = true;
-
+                        // dd( $peopleDailyRecord);
+                        // $delay_arr[] = $firstAfter1400_interval->format('%H h %I m');
+                        // $delay_color = true;
+                        $peopleDailyRecord[$peopleId][$date]['delay_hour'][]= $firstAfter1400_interval->format('%h hours, %i minutes, %s seconds');
+                        $peopleDailyRecord[$peopleId][$date]['delay_display']=true;
+                        dd( $peopleDailyRecord);
 
 
                     }
 
-                    }
+                }
 
 
 
