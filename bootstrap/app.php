7a9dcfa2ba75fb9abaa6d6509571cb3010a707de
ArmineKhachatryan1982 <armine.khachatryan1982@gmail.com>
2025-04-08 14:38:36 +0400

    add in bootstrap/app.php add https://premium-entry-system.trigger.ltd/*

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -30,6 +30,7 @@ return Application::configure(basePath: dirname(__DIR__))
             'https://entrysystem.trigger.ltd/*',
             'http://127.0.0.1:8000/*',
             'http://localhost:8000/*',
+            "https://premium-entry-system.trigger.ltd/*",
 
         ]);
 
